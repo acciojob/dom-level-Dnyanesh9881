ca7e728c6a9d8aee8bebf244fb1ca069a661a90c
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-30 02:08:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let arr=document.getElementsByTagName("li");
+
+for(let i=0;i<arr.length;i++){
+	if(arr[i].id==='level'){
+		alert("The level of the element is: "+i)
+	}
+}
